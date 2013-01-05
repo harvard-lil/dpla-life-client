63261fdd9ae46de4876567d787435b975e7373ae
imakewebthings <caleb@imakewebthings.com>
2013-01-04 22:27:57 -0800

    EOD mid index-books checkin

--- app/views/index.js
+++ app/views/index.js
@@ -1,10 +1,12 @@
 define([
   'underscore',
+  'jquery',
   'settings',
   'views/base',
   'text!templates/index.html',
+  'text!templates/stackview-book.html',
   'stackview'
-], function(_, settings, BaseView, IndexTemplate) {
+], function(_, $, settings, BaseView, IndexTemplate, StackViewBookTemplate) {
 
   var IndexView = BaseView.extend({
     el: '.app-main',
@@ -12,6 +14,9 @@ define([
 
     render: function() {
       BaseView.prototype.render.call(this);
+
+      StackView.get_types().book.template = StackViewBookTemplate;
+      console.log(StackView.get_types().book.template);
       this.$('.stackview').stackView({
         url: settings.get('indexStackURL'),
         query: settings.get('indexSearchTerm'),
