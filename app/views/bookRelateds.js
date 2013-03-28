af2d31bb8877b9f66d50c24eca04bd0e89589301
imakewebthings <caleb@imakewebthings.com>
2013-03-28 14:55:29 -0700

    Related Books section of book page

--- app/views/bookRelateds.js
+++ app/views/bookRelateds.js
@@ -1,33 +1,48 @@
 define([
   'underscore',
+  'jquery',
   'settings',
+  'mediator',
   'views/base',
   'views/stack',
   'text!templates/bookRelateds.html'
-], function(_, settings, BaseView, StackView, BookRelatedsTemplate) {
+], function(
+  _,
+  $,
+  settings,
+  mediator,
+  BaseView,
+  StackView,
+  BookRelatedsTemplate
+) {
 
   var BookRelatedsView = BaseView.extend({
     template: _.template(BookRelatedsTemplate),
 
-    initialize: function(options) {
-      BaseView.prototype.initialize.call(this, options);
-      this.loadNeighbors();
-      this.loadSubjectUnion();
+    events: {
+      'click .single-subject': 'loadSingleSubject',
+      'click .subject-union': 'loadSubjectUnion',
+      'click .subject-intersection': 'loadSubjectIntersection',
+      'click .neighbors': 'loadNeighbors',
+      'click .related-category a': 'setActiveButton',
+      'click .stack-item-link': 'loadPreview'
     },
 
     loadNeighbors: function() {
       var self = this;
 
       var onError = function() {
-        self.$('.on-shelves-with').remove();
+        self.loadStack({
+          data: { num_found:0, docs: [] },
+          ribbon: 'Shelf Neighbors'
+        });
       };
 
       var onSuccess = function(data) {
-        self.subviews.push(new StackView({
-          el: '.on-shelves-with',
+        self.loadStack({
           data: data,
-          ribbon: 'On Shelves With'
-        }));
+          ribbon: 'Shelf Neighbors'
+        });
       };
 
       $.ajax({
@@ -38,14 +53,54 @@ define([
       });
     },
 
-    loadSubjectUnion: function() {
-      this.subviews.push(new StackView({
-        el: '.subject-union',
+    loadSingleSubject: function(event) {
+      var subject = $(event.target).data('subject');
+
+      this.loadStack({
+        url: settings.get('searchURL'),
+        search_type: 'subject',
+        query: subject,
+        ribbon: subject
+      });
+    },
+
+    loadSubjectUnion: function(event) {
+      this.loadStack({
         url: settings.get('searchURL'),
         search_type: 'subject_union',
         query: this.model.get('source_id'),
-        ribbon: 'Subject Union'
-      }));
+        ribbon: 'Share Any Subject'
+      });
+    },
+
+    loadSubjectIntersection: function(event) {
+      this.loadStack({
+        url: settings.get('searchURL'),
+        search_type: 'subject_intersection',
+        query: this.model.get('source_id'),
+        ribbon: 'Share All Subjects'
+      });
+    },
+
+    loadStack: function(options) {
+      _.invoke(this.subviews, 'clear');
+      this.subviews = [new StackView(_.extend({
+        el: '.book-relateds .stack-wrapper'
+      }, options))];
+    },
+
+    setActiveButton: function(event) {
+      this.$('.related-category a').removeClass('active');
+      $(event.target).addClass('active');
+      event.preventDefault();
+    },
+
+    loadPreview: function(event) {
+      var $target = $(event.target).closest('.stack-item');
+      var id = $target.data('stackviewItem')['source_id'];
+
+      mediator.trigger('preview:load', id);
+      event.preventDefault();
     }
   });
 
