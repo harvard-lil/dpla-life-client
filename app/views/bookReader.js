d70be34a60d5e0e5914d5e5e94cd329ca4e74d96
imakewebthings <caleb@imakewebthings.com>
2013-04-08 11:22:25 -0700

    Make book reader fill most of screen

--- app/views/bookReader.js
+++ app/views/bookReader.js
@@ -1,11 +1,20 @@
 define([
   'underscore',
+  'jquery',
   'views/base',
   'text!templates/bookReader.html'
-], function(_, BaseView, BookReaderTemplate) {
+], function(_, $, BaseView, BookReaderTemplate) {
 
   var BookReaderView = BaseView.extend({
-    template: _.template(BookReaderTemplate)
+    template: _.template(BookReaderTemplate),
+
+    render: function() {
+      BaseView.prototype.render.call(this);
+      this.$('.book-reader').css({
+        width: $(window).width() * .85,
+        height: $(window).height() * .85
+      });
+    }
   });
 
   return BookReaderView;
